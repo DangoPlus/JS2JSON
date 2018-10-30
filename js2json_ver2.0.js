ef53fcb1cd884ebce1c1fe574055cde60d8133a4
DangoPlus <depengyu@gmail.com>
2018-10-30 11:45:33 +0800

    update

--- js2json_ver2.0.js
+++ js2json_ver2.0.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 //解析需要遍历的文件夹，我这以E盘根目录为例
-var filePath = path.resolve('./xx');
+var filePath = path.resolve('./ja');
 
 //调用文件遍历方法
 fileDisplay(filePath);
@@ -32,7 +32,10 @@ function fileDisplay(filePath){
                             var jp = require(filedir);
                             const input_jp = {conf:{},i18n: jp};
                             let output_jp = JSON.stringify(input_jp,null,"\t");
-                            fs.writeFile(filename, output_jp);
+                            let filebase = path.basename(filename,'.js');
+                            console.log(filebase);
+                            let outfile = [filebase, '.json'].join('');
+                            fs.writeFile(outfile, output_jp);
                         }
                         if(isDir){
                             fileDisplay(filedir);//递归，如果是文件夹，就继续遍历该文件夹下面的文件
